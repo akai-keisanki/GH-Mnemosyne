f0b1549de754e2cb3d09f997c19e45a667e820ab
AlvsDias <98051617+AlvsDias@users.noreply.github.com>
2024-12-30 14:56:02 -0300

    feat: minimal changes (added minimal frontend features)

---
+++ src/ui/__init__.py
@@ -0,0 +1,23 @@
+import flask
+
+class Mne:
+
+    def __init__(self) -> None:
+
+        ...
+
+        return
+
+
+    def setPages(self) -> None:
+
+        ...
+
+        return
+
+
+    def run(self) -> None:
+
+        ...
+
+        return
