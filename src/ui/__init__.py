29577ffb599bc43a448107ad1e23208ce64ecc9f
AlvsDias <98051617+AlvsDias@users.noreply.github.com>
2025-01-02 10:48:14 -0300

    feat: added API route `/gen/stt` for generating school timetables on backend

--- src/ui/__init__.py
+++ src/ui/__init__.py
@@ -1,4 +1,6 @@
 import flask
+import src.gen as gen
+import json
 
 class Mne:
 
@@ -17,21 +19,70 @@ class Mne:
 
         @self.app.route('/start')
         def start():
+            """
+            Introductory page.
+            """
+
             return 'start page'
 
-        @self.app.route('/mul')
-        def mul():
-            a = flask.request.args.get('a', type = int)
-            b = flask.request.args.get('b', type = int)
-            return f'{a * b}'
+        @self.app.route('/return')
+        def ret():
+            return f'{flask.request.args.get("code", default = 0, type = int)}'
 
-        @self.app.route('/gen/make')
+        @self.app.route('/gen/stt')
         def gen_make():
-            horpd = flask.request.json['schedules per day']
-            days = flask.request.json['weekly working days']
-            profs = flask.request.json['teacher amount']
-            ...
-            return str(flask.request.json['title'])
+            """
+            Generate a school timetable through API.
+
+            Notes
+            -----
+            JSON format:
+            ```
+            {
+                "classrooms": [str],
+                "weekly working day amount": int,
+                "schedules per day": int,
+                "teachers":
+                {
+                    "<teacher>":
+                    {
+                        "schedules per week": int,
+                        "subjects":
+                        {
+                            "<classroom>": [str],
+                            ...
+                        }
+                    },
+                    ...
+                }
+            }
+            ```
+            """
+
+            c = flask.request.json['classrooms']
+            t = []
+            d = []
+            td = []
+
+            turdis = []
+
+            for ti, tc in flask.request.json['teachers'].items():
+                t.append(ti)
+                d.append([])
+
+                turdis = [[] for _ in range(len(c))]
+
+                for s, dis in tc['subjects'].items():
+                    for di in dis:
+                        if di not in d[len(t)-1]: d[len(t)-1].append(di)
+                    turdis[c.index(s)] = [d[len(t)-1].index(di) for di in dis]
+
+                td.append((tc['schedules per week'], turdis))
+
+            r_hor = gen.gen_hors(len(c), flask.request.json['weekly working day amount'], flask.request.json['schedules per day'], td)[0]
+            r_hor = gen.lab_hor(r_hor, c, t, d)
+
+            return str(json.dumps(r_hor))
 
         return
 
