82a7c9948e9fcb599871b527000a08f28b2f81fe
AlvsDias <98051617+AlvsDias@users.noreply.github.com>
2025-01-04 10:05:58 -0300

    fix: trying to fix executable timeout issue (16)

---
+++ app.py
@@ -0,0 +1,14 @@
+from src.ui import *
+
+
+mne : Mne = Mne(__name__)
+
+mne.setRoutes()
+
+
+app = mne.app
+
+
+if __name__ == '__main__':
+
+    mne.run()
---
+++ functions/index.py
@@ -0,0 +1,6 @@
+from src.ui import Mne
+
+mne = Mne(__name__)
+mne.setRoutes()
+
+app = mne.app
